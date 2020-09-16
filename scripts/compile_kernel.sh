7a39449e560f690d52fc8cfef4a6e53eaadd0d3c
jin <Jin>
2020-09-15 21:53:07 -0400

    add scripts

---
+++ scripts/compile_kernel.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -x
+
+#If current mounted, unmount
+sudo umount $MOUNT_DIR
+
+#Compile the kernel
+cd $KERN_SRC
+
+
+#Enable the KVM mode in your kernel config file
+sudo make x86_64_defconfig
+sudo make kvmconfig 
+
+#Compile the kernel with '-j' (denotes parallelism) in sudo mode
+sudo make $PARA
+#sudo make modules
+#sudo make modules_install
+#sudo make install
+
+y="5.8.9"
+
+sudo cp ./arch/x86/boot/bzImage $KERNEL/vmlinuz-$y
+sudo cp System.map $KERNEL/System.map-$y
+sudo cp .config $KERNEL/config-$y
+
