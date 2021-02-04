d491301584b800eb0ba6391a90ab75d646de2ae4
codingjin <55301130+codingjin@users.noreply.github.com>
2021-02-04 00:13:41 -0500

    Update run_qemu.sh

--- scripts/run_qemu.sh
+++ scripts/run_qemu.sh
@@ -2,7 +2,7 @@
 set -x
 
 #Launching QEMU
-sudo qemu-system-x86_64 -kernel $KERNEL/vmlinuz-$VER -hda $QEMU_IMG_FILE -append "root=/dev/sda rw" -smp 4 --curses -m $QEMUMEM  
+sudo qemu-system-x86_64 -kernel $KERNEL/vmlinuz-$VER -hda $QEMU_IMG_FILE -append "root=/dev/sda rw" -smp 4 --curses -m $QEMUMEM -enable-kvm 
 #sudo qemu-system-x86_64 -kernel $KERNEL/vmlinuz-$VER -hda $QEMU_IMG_FILE -append "root=/dev/sda rw" --curses -m $QEMUMEM  
 
 set +x
