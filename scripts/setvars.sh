41c3da9a97b8c5b5a4cc87604e5cf25646a99d3c
codingjin <55301130+codingjin@users.noreply.github.com>
2021-02-05 23:40:47 -0500

    Update setvars.sh

--- scripts/setvars.sh
+++ scripts/setvars.sh
@@ -5,11 +5,11 @@ set -x
 export BASE=$PWD
 export OS_RELEASE_NAME="trusty"
 export QEMU_IMG=$BASE
-export KERN_SRC=$BASE/linux-5.10.12
+export KERN_SRC=$BASE/linux-5.10.13
 export KERNEL=$BASE/KERNEL
 #CPU parallelism
 export PARA="-j64"
-export VER="5.10.12"
+export VER="5.10.13"
 export QEMU_IMG_FILE=$PWD/qemu-image.img
 export MOUNT_DIR=$PWD/mountdir
 export QEMUMEM="33000M"
